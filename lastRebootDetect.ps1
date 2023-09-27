a815154b6936ab55fe8801fb69916a5a29a2d0cb
Bikesh Mahato <65081900+ASCII2741@users.noreply.github.com>
2023-09-28 08:00:24 +1000

    main

--- lastRebootDetect.ps1
+++ lastRebootDetect.ps1
@@ -1,59 +1,30 @@
-function Send-ToastNotification
-{
-    [CmdletBinding()]
-    [OutputType([string])]
-    Param
-    (
-        [Parameter(Mandatory=$true,
-                   ValueFromPipeline=$true,
-                   ValueFromPipelineByPropertyName=$true,
-                   Position=0)]
-        [string]
-        $Title,
-        [Parameter(Mandatory=$true,
-                   ValueFromPipeline=$true,
-                   ValueFromPipelineByPropertyName=$true,
-                   Position=1)]
-        [string]
-        $Message,
-        [Parameter(Mandatory=$true,
-                   ValueFromPipeline=$true,
-                   ValueFromPipelineByPropertyName=$true,
-                   Position=2)]
-        [string]
-        $Advice,
-        [Parameter(Mandatory=$true,
-                   ValueFromPipeline=$true,
-                   ValueFromPipelineByPropertyName=$true,
-                   Position=3)]
-        [int]
-        $DaysThreshold
-    )
-    $Toast = @"
-    <toast launch="app-defined-string">
-        <visual>
-            <binding template="ToastGeneric">
-                <text>$Title</text>
-                <text>$Message</text>
-                <text>$Advice</text>
-            </binding>
-        </visual>
-        $Actions
-    </toast>
-"@
-    $AppID = $Text_AppName
-    $AppDisplayName = $Text_AppName
-    Register-NotificationApp -AppID $Text_AppName -AppDisplayName $Text_AppName
-    # Toast creation and display
-    $Load = [Windows.UI.Notifications.ToastNotificationManager, Windows.UI.Notifications, ContentType = WindowsRuntime]
-    $Load = [Windows.Data.Xml.Dom.XmlDocument, Windows.Data.Xml.Dom.XmlDocument, ContentType = WindowsRuntime]
-    $ToastXml = New-Object -TypeName Windows.Data.Xml.Dom.XmlDocument
-    $ToastXml.LoadXml($Toast.OuterXml)    
-    # Display the Toast
-    [Windows.UI.Notifications.ToastNotificationManager]::CreateToastNotifier($AppID).Show($ToastXml)
+# Set the path to store the last reboot time
+$RebootTimeFile = "C:\Windows\Temp\LastRebootTime.txt"
+
+# Function to get the last reboot time from the file
+Function Get-LastRebootTime {
+    if (Test-Path $RebootTimeFile) {
+        $lastRebootTime = Get-Content $RebootTimeFile | Get-Date
+        return $lastRebootTime
+    }
+    return $null
 }
-$Actions = @"
-  <actions>
-        <action activationType="protocol" arguments="Dismiss" content="Dismiss" />
-   </actions>    
-"@
+
+# Function to save the last reboot time to the file
+Function Save-LastRebootTime {
+    $Last_reboot | Out-File -FilePath $RebootTimeFile -Force
+}
+
+# Get the last reboot time from the file or CIMInstance
+$Last_reboot = Get-LastRebootTime
+if ($Last_reboot -eq $null) {
+    $Last_reboot = Get-CimInstance Win32_OperatingSystem | Select-Object -ExpandProperty LastBootUpTime
+    Save-LastRebootTime
+}
+
+# Calculate system uptime
+$Current_Date = Get-Date
+$Diff_boot_time = $Current_Date - $Last_reboot
+$Boot_Uptime_Days = $Diff_boot_time.Days
+
+# Rest of the script remains the same...
